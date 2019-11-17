a78b8175a200c1cef927ccda4b031d774581b755
jimmy <jntran@wpi.edu>
2019-11-17 18:30:04 -0500

    Unique lists for senders/destinations for IP/Ethernet. Checks for ethernet headers in the pcap file. .. and i think that's it.

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <pcap.h>
+#include <string>
 #include <net/ethernet.h>
 #include <netinet/ip.h>
 #include <netinet/ether.h>
@@ -7,8 +8,16 @@
 #include <netinet/udp.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
+#include <unordered_map>
 
 using namespace std;
+
+unordered_map<string, int> ethernetDest;
+unordered_map<string, int> ethernetSrc;
+unordered_map<string, int> ipDest;
+unordered_map<string, int> ipSrc;
+unordered_map<u_int, int> udpDest;
+unordered_map<u_int, int> udpSrc;
 
 bool isAscii(int testVal) {
     bool isReturn = testVal==10 || testVal==11 || testVal==13;
@@ -33,16 +42,28 @@ void handler(u_char* user, const struct pcap_pkthdr* header, const u_char* packe
 
     //check if the packet is IP
     if(ntohs(eH->ether_type) == ETHERTYPE_IP) {
+        string eDest(ether_ntoa((const struct ether_addr *) &eH->ether_dhost));
+        string eSrc(ether_ntoa((const struct ether_addr *) &eH->ether_shost));
+        ethernetDest[eDest]++;
+        ethernetSrc[eSrc]++;
 
-        printf("Dest = %s\n", ether_ntoa((const struct ether_addr *) &eH->ether_dhost));
-        printf("Source = %s\n", ether_ntoa((const struct ether_addr *) &eH->ether_shost));
+        //printf("Count: %i\n", ethernetDest[ether_ntoa((const struct ether_addr *) &eH->ether_dhost)]);
+
+        //printf("Dest = %s\n", ether_ntoa((const struct ether_addr *) &eH->ether_dhost));
+        //printf("Source = %s\n", ether_ntoa((const struct ether_addr *) &eH->ether_shost));
         //read data into ipHeader, pointer offset by the ethernet header
         headerSize = sizeof(struct ether_header);
         ipH = (struct ip*)(packetPointer + headerSize);
 
         //convert binary addresses into string
+
         inet_ntop(AF_INET, &(ipH->ip_src), src, INET_ADDRSTRLEN);
         inet_ntop(AF_INET, &(ipH->ip_dst), dst, INET_ADDRSTRLEN);
+
+        string source(src);
+        string dest(dst);
+        ipSrc[source]++;
+        ipDest[dest]++;
 
         //check ip header for TCP or UDP
         if (ipH->ip_p == IPPROTO_TCP) {
@@ -70,13 +91,13 @@ void handler(u_char* user, const struct pcap_pkthdr* header, const u_char* packe
                 }
             }
 
-            printf("%s,",ctime((const time_t*)&header->ts.tv_sec));
+            //printf("%s,",ctime((const time_t*)&header->ts.tv_sec));
             //print data
             //for (int i = 0; i < len; i++) {cout << (char) data[i];}
-            cout << endl << "_______________________________________________________________________________________________"<<endl;
-            cout << endl << "Source: " << src << ":" << srcPort << endl;
-            cout << "Destination: " << dst << ":" << dstPort << endl;
-            cout << "_______________________________________________________________________________________________"<<endl;
+            //cout << endl << "_______________________________________________________________________________________________"<<endl;
+            //cout << endl << "Source: " << src << ":" << srcPort << endl;
+            //cout << "Destination: " << dst << ":" << dstPort << endl;
+            //cout << "_______________________________________________________________________________________________"<<endl;
 
 
             //cout << output;
@@ -92,18 +113,43 @@ void handler(u_char* user, const struct pcap_pkthdr* header, const u_char* packe
     }
 }
 
-int main() {
-    pcap_t *pcapture;
-    char ebuf[PCAP_ERRBUF_SIZE];
 
-    pcapture = pcap_open_offline("cap.pcap", ebuf);
+void printLists(){
+    cout << "Ethernet Destinations!\n";
+    for(auto elem: ethernetDest){ cout << "[Destination: "<< elem.first << "] " << "[Count: " << elem.second << "]\n";}
+    cout << "Ethernet Sources!\n";
+    for(auto elem: ethernetSrc){ cout << "[Source: "<< elem.first << "] " << "[Count: " << elem.second << "]\n";}
+    cout << "IP Destinations!\n";
+    for(auto elem: ipDest){ cout << "[Dest: "<< elem.first << "] " << "[Count: " << elem.second << "]\n";}
+    cout << "IP Sources!\n";
+    for(auto elem: ipSrc){ cout << "[Source: "<< elem.first << "] " << "[Count: " << elem.second << "]\n";}
+}
+int main(int argc, char const *argv[]) {
+    if(argc > 2 || argc < 2){
+        printf("Invalid arguments. Only arg should be file.\n");
+        exit(0);
+    } else {
+        std::string fileName = argv[1];
+        pcap_t *pcapture;
+        char ebuf[PCAP_ERRBUF_SIZE];
 
-    if(pcapture == NULL) {
-        cout << "read in failed" << endl;
-    }
+        pcapture = pcap_open_offline(fileName.c_str(), ebuf);
 
-    if(pcap_loop(pcapture, 0, handler, NULL) < 0) {
-        cout << "handler read failed" << endl;
-        return 1;
+        if (pcapture == NULL) {
+            cout << "read in failed" << endl;
+            exit(0);
+        }
+
+        if(pcap_datalink(pcapture) != DLT_EN10MB){
+            cout << "Not captured using Ethernet\n";
+            exit(0);
+        }
+
+        if (pcap_loop(pcapture, 0, handler, NULL) < 0) {
+            cout << "handler read failed" << endl;
+            return 1;
+        }
+        printLists();
+
     }
 }
