49a56233010e70c8ef358a86c740b71dc590ad10
jimmy <jntran@wpi.edu>
2019-12-03 17:09:08 -0500

    Made function to "create" packets. A refactor that took too long. ;/

--- Project3/server2.cpp
+++ Project3/server2.cpp
@@ -51,18 +51,24 @@ struct datagram{
 
 };
 
-unsigned short createCSum(unsigned short *buffer, int words){
-
-    unsigned long sum;
-    for(sum=0; words>0; words--){
-        sum+= *buffer++;
-        sum = ( sum >> 16) + (sum &0xffff);
-        sum += (sum >> 16);
-        return (unsigned short)(~sum);
-    }
-}
 
 unordered_map<string, string> routingTable;
+
+void createPacket(datagram *packet, in_addr_t dst, in_addr_t src, u_int16_t ttl){
+    
+    packet->iph.iph_ihl = 5;
+    packet->iph.iph_ver = 4;
+    packet->iph.iph_tos = 0;
+    packet->iph.iph_len = sizeof(struct datagram);
+    packet->iph.iph_ident = htons(54321);
+    packet->udph.src = htons(8080);
+    packet->udph.dest = htons(8080);
+    packet->iph.iph_ttl = ttl;
+    packet->iph.iph_src = src;
+    packet->iph.iph_dest = dst;
+    //inet_pton(AF_INET, "192.69.69.69", &packet->iph.iph_dest);
+
+}
 
 int main(int argc, char const *argv[]) {
 
@@ -71,8 +77,16 @@ int main(int argc, char const *argv[]) {
     routingTable.insert(pair<string,string>(s0,s1));
     routingTable.insert(pair<string,string>(string("a"),string("b")));
     routingTable[string("test")]=string("value");
+    int sockfd;
+    char buffer[4096], *data;
+    struct datagram *packet = (struct datagram *)malloc(sizeof(struct datagram));
+    //struct ip_header *iph = (struct ip_header*)malloc(sizeof(struct ip_header));
+    //struct udp_header *udph = (struct udp_header *)malloc(sizeof(struct udp_header));
 
-    for( auto str = routingTable.begin(); str != routingTable.end(); ++str) {
+    const char *msg = "hello im the server\n";
+    bool isClient = false;
+
+/*    for( auto str = routingTable.begin(); str != routingTable.end(); ++str) {
         cout << str->first; //key output
         string& value = str-> second;
         cout << ":" << value << endl;
@@ -110,38 +124,7 @@ int main(int argc, char const *argv[]) {
     //string to lookup received from packet
     string test5 = "8.8.8.8";
 
-    cout << test5 << " is " << routingTable.at(test5) << endl;
-
-    //if (router), (IP:oIP), ... -> routingTable.insert
-    //router should allow connections from INADDR_ANY
-    //once router receives a connection, it should wait for a packet size, followed by the packets
-    //packets should contain a overlay address
-    //realADDRstr = routingTable.at(overlayIP)
-    //create sock_addr* saddr for connection
-    //saddr.sin_addr = inet_pton(realADDRstr) -> INET_ADDR
-    //bind to saddr, create new overlay packet with overlayIP
-    //sendTo(bindsocket, newPkt, sizeof(datagram), 0, (struct sock_addr) &saddr, sizeof(&saddr))
-
-    //if (client), (serverIP)
-    //wait for pkt size on one thread, read source overlay IP
-    //check if dest overlay IP addr inside packet is correct for the client, else drop it
-    //check if the src of the received packet was the router, if not, drop it
-    //wait for packets and check for correct sizes and continuity and order
-    //check if sourceOverlayIP.bin exists, if not, create it
-    //append to sourceOverlayIP.bin
-
-    //on other thread:
-    //scan for files that fit correct shape (regex?)
-    //read in overlay addr from file name
-    //read in size of packet, create a packet to send the size
-    //send the file in groups of 1000 bytes - any info we need to send
-    //
-
-
-
-
-
-
+    cout << test5 << " is " << routingTable.at(test5) << endl;*/
 
     struct sockaddr_in server_addr, client_addr, sa_test_dummy;
     std::string routerIp = "10.0.2.15";
@@ -179,13 +162,13 @@ int main(int argc, char const *argv[]) {
                         std::cout << value << '\n';
                         counter++;
                     }
-                    printf("Hi?\n");
                 }
                 if(counter%2!=0){
                     printf("There is a lonely IP address somewhere..how depressing.\n");
                     exit(1);
                 }
             } else if(curr_cmd.compare("--HOST") == 0) {
+                isClient = true;
                 while(std::getline(ss, token, ',')) {
                     if(inet_pton(AF_INET, token.c_str(), &sa_test_dummy.sin_addr) != 1 && counter !=2) {
                         printf("You gave an invalid IP address somewhere :(\n");
@@ -236,14 +219,37 @@ int main(int argc, char const *argv[]) {
         exit(1);
     }
 
-    //printf("%s\n%s\n%i\n", routerIp.c_str(), hostIp.c_str(), ttl);
-    int sockfd;
-    char buffer[4096], *data;
-    struct datagram *packet = (struct datagram *)malloc(sizeof(struct datagram));
-    //struct ip_header *iph = (struct ip_header*)malloc(sizeof(struct ip_header));
-    //struct udp_header *udph = (struct udp_header *)malloc(sizeof(struct udp_header));
 
-    const char *msg = "hello im the server\n";
+    //I'm host stuff c:
+    if(isClient){
+        //if (client), (serverIP)
+        //wait for pkt size on one thread, read source overlay IP
+        //check if dest overlay IP addr inside packet is correct for the client, else drop it
+        //check if the src of the received packet was the router, if not, drop it
+        //wait for packets and check for correct sizes and continuity and order
+        //check if sourceOverlayIP.bin exists, if not, create it
+        //append to sourceOverlayIP.bin
+
+        //on other thread:
+        //scan for files that fit correct shape (regex?)
+        //read in overlay addr from file name
+        //read in size of packet, create a packet to send the size
+        //send the file in groups of 1000 bytes - any info we need to send
+        //
+    } else { //I'm a router :c
+
+        //if (router), (IP:oIP), ... -> routingTable.insert
+        //Decrement TTL
+        //router should allow connections from INADDR_ANY
+        //once router receives a connection, it should wait for a packet size, followed by the packets
+        //packets should contain a overlay address
+        //realADDRstr = routingTable.at(overlayIP)
+        //create sock_addr* saddr for connection
+        //saddr.sin_addr = inet_pton(realADDRstr) -> INET_ADDR
+        //bind to saddr, create new overlay packet with overlayIP
+        //sendTo(bindsocket, newPkt, sizeof(datagram), 0, (struct sock_addr) &saddr, sizeof(&saddr))
+    }
+    //printf("%s\n%s\n%i\n", routerIp.c_str(), hostIp.c_str(), ttl);
 
 
 
@@ -254,6 +260,7 @@ int main(int argc, char const *argv[]) {
 
     //memset(iph, 0 , sizeof(ip_header));
     memset(buffer, 0 , 4096);
+    memset(packet, 0, sizeof(datagram));
     memset(&server_addr, 0, sizeof(server_addr));
     memset(&client_addr, 0, sizeof(client_addr));
 
@@ -261,21 +268,11 @@ int main(int argc, char const *argv[]) {
     server_addr.sin_addr.s_addr = inet_addr(routerIp.c_str());
     server_addr.sin_port = htons(34567);
 
+    createPacket(packet, client_addr.sin_addr.s_addr, server_addr.sin_addr.s_addr, ttl);
+//    data = buffer;
+//    strcpy(data, "HereIsSomeData");
 
-    data = buffer;
-    strcpy(data, "HereIsSomeData");
 
-    packet->iph.iph_ihl = 5;
-    packet->iph.iph_ver = 4;
-    packet->iph.iph_tos = 0;
-    packet->iph.iph_len = sizeof(struct datagram);
-    packet->iph.iph_ident = htons(54321);
-    packet->iph.iph_ttl = ttl;
-    packet->iph.iph_src = server_addr.sin_addr.s_addr;
-    inet_pton(AF_INET, "192.69.69.69", &packet->iph.iph_dest);
-
-    packet->udph.src = htons(8080);
-    packet->udph.dest = htons(8080);
 
     if(bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) <0) {
         perror("bind call failed\n");
@@ -283,8 +280,7 @@ int main(int argc, char const *argv[]) {
 
 
     strcpy(packet->data, "HereIsSomeData");
-    //printf("%i\n", packet->iph.iph_ihl);
-    //printf("%s\n", packet->data);
+
     int one = 1;
     const int *val = &one;
     //if(setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, val, sizeof(one))<0){printf("Warning: HDRINCL not set\n");}
